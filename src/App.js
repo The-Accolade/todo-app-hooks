33a38d7e1ddc3e239802bc98b5b601f9aef427f5
funshofatoye <fatoyefunsho@gmail.com>
2020-07-03 15:02:26 +0100

    first attempt with react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  // const [buttonText, changeButtonText] = useState('undo');
+  // const changeText = text => changeButtonText(text)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button
+          onClick={() => {
+            completeTodo(index);
+            // changeText(completeTodo? 'Completed' : 'Undo' ) I want to make the button toggle to undo that removes the styling
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {/* {buttonText} here the text of the button is supposed to toggle between 'completed' and 'undo' and  carry out the action*/}
+          Completed
+        </button>
+        <button
+          onClick={() => {
+            removeTodo(index);
+          }}
+        >
+          x
+        </button>
+      </div>
     </div>
   );
 }
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+ 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        className="input"
+         placeholder="Add Todo" 
+         value={value}
+         onChange={e => setValue(e.target.value)} />  
+     </form>
+  )
+}
+
+function App() {
+    const [todos, setTodos] = useState([
+      {
+        text: 'Learn React',
+        isCompleted: false
+      },
+      {
+        text: 'Meet with friends',
+        isCompleted: false
+      },
+      {
+        text: 'Create a todo app with react hooks',
+        isCompleted: false
+      }
+    ]);
+
+    const addTodo = text => {
+      const newTodos = [...todos, { text }];
+      setTodos(newTodos);
+    }
+    const completeTodo = (index)=> {
+      const newTodos = [...todos];
+      newTodos[index].isCompleted = true;
+      setTodos(newTodos);
+    }
+    const removeTodo = (index)=> {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1)
+      setTodos(newTodos);
+    }
+    
+    return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    );
+}
+
 
 export default App;
